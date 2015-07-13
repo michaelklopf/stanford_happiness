b0f72cf1ba3f2e9ef7d749e919a415898df4316b
Michael Klopf <klopf.michael@gmail.com>
2015-07-13 09:22:22 +0200

    Added code from lesson 5 video.

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Michael on 13.07.15.
+//  Copyright © 2015 Michael Klopf. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+    
+}
